14a04339d3cff91e6006c5b537e0b8c6cf9acf2c
muhamedh <muhammed.hamzic@gmail.com>
2022-04-16 13:35:29 +0200

    minor router bug fixes + mkdir_c created

--- shell.c
+++ shell.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int loop = 1;
 
@@ -60,8 +62,38 @@ void username_hostname(){
 	printf("$ ");
 }
 
-void mkdir(){
+void mkdir_c(char flags[10], int f_size, char name[128], int n_size){
+	char dir[PATH_MAX]; // PATH_MAX - defines the longest path a direcotry can be
+	getcwd(dir, sizeof(dir)); // getcwd - gives us the absolute pathname of the current working directory
 
+	char slash[129] = "/"; // creates a new string slighty bigger than name
+	strcat(slash,name); // in order to concatenate res -> /name_of_our_new_dir
+
+	if(name[0] != '\n'){ // prevents creating a '' named directory
+
+		mkdir(strcat(dir,slash), 0777); // make the dir with mkdir function
+		//0777 -> -rwxrwxrwx read,write & execute for owner, group and others
+		
+	}
+
+
+	for( int i = 0;i < f_size;i++){
+		if(flags[i] == 'v'){
+			name[strlen(name)-1] = '\'';
+			printf("mkdir: created directory '%s\n", name);
+		}
+		if(flags[i] == 'h'){
+			printf("Usage mkdir [OPTION] ... DIRECTORY...\n");
+			printf("Create the DIRECTORY, if they do not already exist\n");
+			printf("-v print a message for a created directory\n");
+			printf("-V print the current version\n");
+			printf("-h show the help page\n");
+		}
+		if(flags[i] == 'V'){
+			printf("current version mkdir : v0.0.1\n");
+		}
+	}
+	
 }
 
 void ls(){
@@ -75,17 +107,21 @@ void uptime(){
 void sl(){
 
 }
-
+/**
+ * Function used for routing user input
+*/
 void router(char input[1024]){
-	char function[10];
-	char flags[10];
+
+	char function[10] = "";
+	char flags[10] = "";
 	int output;
-	char file_name[128];
+	char file_name[128] = "";
 	int fun_counter = 0;
 	int flag_counter = 0;
+	int name_counter = 0;
 
 	for(int i = 0; i < strlen(input); i++){
-		if(input[i] != ' '){
+		if(input[i] != ' ' || (int)input[i] == 0 ){
 			function[fun_counter] = input[i];
 			fun_counter++;
 		}
@@ -100,11 +136,26 @@ void router(char input[1024]){
 			flag_counter++;
 		}
 	}
-	
+
+	for(int i = fun_counter + 2*flag_counter + 1; i < strlen(input); i++){
+		if(input[i] == '>'){
+			output = 1;
+		}
+		if(input[i] != '>' && (input[i] != ' ' || (int)input[i] == 0)){
+			file_name[name_counter] = input[i];
+			name_counter++;
+		}
+	}
+
+	if(output == 1){
+		// handle redirecting to file
+		// todo
+	}
+
 	function[strcspn(function, "\n")] = 0;
 
 	if(strcmp(function, "mkdir") ==  0){
-		mkdir();
+		mkdir_c(flags, flag_counter, file_name, name_counter);
 	}
 
 	else if(strcmp(function, "ls") == 0){
@@ -126,22 +177,30 @@ void router(char input[1024]){
 	else if(strcmp(function, "exit") == 0){
 		loop = 0;
 	}
-	
-	else{
+	else if((int)function[0] != 0){ // tests if the first entered char is not a new line 
 		printf("%s: command not found\n", function);
 	}
 
 }
 int main(void){
 	printf("%c]0;%s%c", '\033', "Medo", '\007');
-	
-	system("clear");
+	/*
+	* The system("clear") is included in stdlib.h and works only in Linux based systems
+	*/
 
+	system("clear");
+	/*
+	* The input char array for storing user input
+	*/
 	char input[1024];
 
 	while(loop){
 
 	username_hostname();
+
+	/*
+	* Even if user outputs is larger than the array size, fgets will handle the overflow properly
+	*/
 	fgets(input, 1024, stdin);
 
 	router(input);
